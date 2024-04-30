ba13c7c49a9aa80744893fe26c7a84b121393841
Teodora Hristova <teodoravhristova@gmail.com>
2024-04-30 11:56:39 +0300

    ref: add-remove element test

--- test/specs/interactions-part1/add-remove-element.spec.js
+++ test/specs/interactions-part1/add-remove-element.spec.js
@@ -1,41 +1,44 @@
-import { assert } from 'chai'
-import { browser } from '@wdio/globals'
-import { getHeaderTitle } from '../../../src/help-functions.js'
-
+/* eslint-disable no-undef */
 describe('Add/Remove Element exercise', () => {
-  it('Should Add/Remove element', async () => {
+  const { addRemoveElementHeader, addElementButton, deleteElementButton } = global.config.selectors
+  before('Open browser', async () => {
     await browser.url('https://the-internet.herokuapp.com/add_remove_elements/')
+    it('Should Add/Remove element', async () => {
+      // Verify the page header is "Add/Remove Elements"
+      const expectedHeaderTitle = 'Add/Remove Elements'
+      await $(addRemoveElementHeader).waitForDisplayed()
+      const actualHeaderTitle = await getElementText(addRemoveElementHeader)
+      assert.equal(actualHeaderTitle, expectedHeaderTitle, `Page header should be "${expectedHeaderTitle}"`)
+      // Confirm the "Add Element" button is present, while the "Delete" button is not
+      const addButton = await $(addElementButton)
+      assert.isTrue(await addButton.isDisplayed(), 'Add Element button should be displayed')
 
-    // Verify the page header is "Add/Remove Elements"
-    const expectedHeaderTitle = 'Add/Remove Elements'
-    const actualHeaderTitle = await getHeaderTitle(expectedHeaderTitle)
-    assert.equal(actualHeaderTitle, expectedHeaderTitle, `Page header should be "${expectedHeaderTitle}"`)
+      assert.isFalse(await $(deleteElementButton).isExisting(), 'Delete button should not exist on the page')
 
-    // Confirm the "Add Element" button is present, while the "Delete" button is not
-    const addButton = await browser.$('button=Add Element')
-    const isDisplayed = await addButton.waitForDisplayed()
-    assert.isTrue(isDisplayed, 'Add Element button should be displayed')
+      // Click the "Add Element" button 10 times.
+      await addButton.waitForClickable()
+      const numberOfExecutions = 10
+      for (let i = 0; i < numberOfExecutions; i++) {
+        await addButton.click()
+      }
 
-    let deleteButtonExists = await browser.$('button=Delete').isExisting()
-    assert.isFalse(deleteButtonExists, 'Delete button should not exist on the page')
+      // Assert that there are 10 delete buttons displayed
+      const deleteButtons = await $$(deleteElementButton)
+      assert.strictEqual(deleteButtons.length, numberOfExecutions, `Expected ${numberOfExecutions} delete buttons to be displayed`)
 
-    // Click the "Add Element" button 10 times.
-    for (let i = 0; i < 10; i++) {
-      await addButton.click()
-    }
-
-    // Assert that there are 10 delete buttons displayed
-    const deleteButtons = await browser.$$('button=Delete')
-    assert.strictEqual(deleteButtons.length, 10, 'Expected 10 delete buttons to be displayed')
-
-    // Click each "Delete" button and confirm that the number of buttons reduces by one each time.
-    for (let i = 0; i < deleteButtons.length; i++) {
-      await deleteButtons[i].click()
-      const remainingDeleteButtons = await browser.$$('button=Delete')
-      assert.strictEqual(remainingDeleteButtons.length, 10 - i - 1, `Expected ${10 - i - 1} delete buttons remaining after clicking button number ${i + 1}`)
-    }
-    // Confirm that no "Delete" buttons are left on the page.
-    deleteButtonExists = await browser.$('button=Delete').isExisting()
-    assert.isFalse(deleteButtonExists, 'Delete button should not exist on the page')
+      // Click each "Delete" button and confirm that the number of buttons reduces by one each time.
+      for (let i = 0; i < deleteButtons.length; i++) {
+        await deleteButtons[i].waitForClickable()
+        await deleteButtons[i].click()
+        const remainingDeleteButtons = await $$(deleteElementButton)
+        assert.strictEqual(
+          remainingDeleteButtons.length,
+          numberOfExecutions - i - 1,
+          `Expected ${numberOfExecutions - i - 1} delete buttons remaining after clicking button number ${i + 1}`,
+        )
+      }
+      // Confirm that no "Delete" buttons are left on the page.
+      assert.isFalse(await $(deleteElementButton).isExisting(), 'Delete button should not exist on the page')
+    })
   })
 })
