d5483156efcee2e2601b315b22680939e930bf32
Teodora Hristova <teodoravhristova@gmail.com>
2024-05-03 11:10:05 +0300

    add: pom

---
+++ test/pageobjects/addelement.page.js
@@ -0,0 +1,13 @@
+export class AddElementPage {
+  get pageHeader() {
+    return $('h3=Add/Remove Elements')
+  }
+
+  get addElementButton() {
+    return $('button=Add Element')
+  }
+
+  get deleteElementButton() {
+    return $('button=Delete')
+  }
+}
