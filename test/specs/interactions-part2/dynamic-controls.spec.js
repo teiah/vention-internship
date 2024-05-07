1fcd33c53255aeafe793769b48fd395c053d5947
Teodora Hristova <teodoravhristova@gmail.com>
2024-05-07 16:48:38 +0300

    add: dynamic controls test

---
+++ test/specs/interactions-part2/dynamic-controls.spec.js
@@ -0,0 +1,30 @@
+import DynamicControlsPage from '../../pageobjects/dynamic-controls.page.js'
+import { assert } from 'chai'
+
+describe('Dynamic controls exercise', function () {
+  it('Should interact with dynamic controls', async function () {
+    await DynamicControlsPage.open()
+
+    // Click on the 'Remove' button next to a checkbox.
+    await DynamicControlsPage.removeButton.click()
+
+    // Wait until the message “It’s gone” appears.
+    await DynamicControlsPage.confirmationMessage.waitForDisplayed({ timeout: 10000 })
+
+    // Verify that the checkbox is no longer present.
+    assert.isFalse(await DynamicControlsPage.checkbox.isExisting(), 'Chechbox should not exist on the page')
+
+    // Locate an input field.
+    // Ensure that the input field is disabled.
+    assert.isFalse(await DynamicControlsPage.inputField.isEnabled(), 'Input field should be initialy disabled.')
+
+    // Click a button to enable the input.
+    await DynamicControlsPage.enableButton.click()
+
+    // Wait for the message “It's enabled!” to appear.
+    await DynamicControlsPage.confirmationMessage.waitForDisplayed({ timeout: 10000 })
+
+    // Confirm that the input field is enabled.
+    assert.isTrue(await DynamicControlsPage.inputField.isEnabled(), 'Input field should be enabled.')
+  })
+})
