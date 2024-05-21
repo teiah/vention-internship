b4bab978c698604e089ff571a2cfe6b985363af8
Teodora Hristova <teodoravhristova@gmail.com>
2024-05-21 15:42:56 +0300

    add: select method for dropdown element

--- framework/elements/Dropdown.js
+++ framework/elements/Dropdown.js
@@ -1,4 +1,10 @@
 import BaseElement from '../BaseElement'
 
-class Dropdown extends BaseElement {}
+class Dropdown extends BaseElement {
+  async selectByVisibleText(text) {
+    const element = await this.getElement(this.selector)
+    element.click()
+    await element.selectByVisibleText(text)
+  }
+}
 export default Dropdown
