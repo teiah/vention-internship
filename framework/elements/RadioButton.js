8400932a536a340c1d13366ea4b023c1b1410663
Teodora Hristova <teodoravhristova@gmail.com>
2024-05-21 15:18:25 +0300

    add: isSelected method for radio button

--- framework/elements/RadioButton.js
+++ framework/elements/RadioButton.js
@@ -1,4 +1,8 @@
 import BaseElement from '../BaseElement'
 
-class RadioButton extends BaseElement {}
+class RadioButton extends BaseElement {
+  async isSelected() {
+    return await this.getElement().isSelected()
+  }
+}
 export default RadioButton
